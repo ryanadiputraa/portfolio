41c2cd14016a27097bb9e2cd0670cfd6d87530de
Ryan Adi Putra <ryannadiputraa@gmail.com>
2023-12-30 18:38:49 +0800

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import { Raleway } from 'next/font/google';
+
+import './globals.css';
+
+const raleway = Raleway({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Ryan Adi Putra',
+  description: 'Software Engineer portfolio website',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={raleway.className}>{children}</body>
+    </html>
+  );
+}
