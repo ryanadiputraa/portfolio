9b878633f9c246e3dd6690b79aca56710a76acac
Ryan Adi Putra <ryannadiputraa@gmail.com>
2023-12-30 20:48:49 +0800

    feat: header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Raleway } from 'next/font/google';
 
+import { Header } from './components/header';
+
 import './globals.css';
 
 const raleway = Raleway({ subsets: ['latin'] });
@@ -13,7 +15,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={raleway.className}>{children}</body>
+      <body className={`${raleway.className} bg-white`}>
+        <Header />
+        <main className="pt-16 px-[4%] sm:px-4 text-base">{children}</main>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,18 @@ const config: Config = {
     './app/components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    colors: {
+      white: '#FAFAF9',
+      'white-grey': '#D9D9D9',
+      black: '#0E0E0C',
+      'soft-black': '#262626',
+      accent: '#8C8C73',
+      'accent-light': '#AEAE9D',
+      'light-grey': '#666666',
+      grey: '#4D4D4D',
+    },
+  },
   plugins: [],
 };
 export default config;
