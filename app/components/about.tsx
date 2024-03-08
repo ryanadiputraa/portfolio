daf1588aa03d100c9db2be4936ea079d5175baad
Ryan Adi Putra <ryannadiputraa@gmail.com>
2024-03-08 09:19:54 +0800

    refactor: experience section

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -10,30 +10,27 @@ interface Experience {
   company: string;
   time: string;
   position: string;
-  description: string;
+  skills: string[];
 }
 
 const experiences: Experience[] = [
   {
     company: 'Garena Indonesia',
-    time: 'Feb 2022 - Present',
+    time: 'Sep 2022 - Present',
     position: 'Frontend Engineer',
-    description:
-      'Developed regional and multi-regional in-game web events using ReactJS. Involved in research and improvement of web development in company such as TeleportHQ for landing page automation & multi language meta tags script automation.',
+    skills: ['ReactJS', 'ReduxJS', 'Micro Frontend', 'Javascript', 'Docker', 'SCSS'],
   },
   {
     company: 'Dicoding Academy (Kampus Merdeka)',
     time: 'Feb 2022 – Aug 2022',
     position: 'Multi Platform and Backend',
-    description:
-      'Collaborated in teams to Develop a multi platform mobile app using Flutter and NodeJS for backend to simplify shopping on physical stores and help owner manage items with generated QR Codes for each items.',
+    skills: ['Flutter', 'NodeJS', 'Express', 'TypeScript', 'MongoDB', 'Dart'],
   },
   {
     company: 'Glints Academy (Kampus Merdeka)',
     time: 'Aug 2021 – Feb 2022',
     position: 'International Internship Student',
-    description:
-      'Developed a simple web application to learn to work in a team and improve code quality by learning clean code by being involved in code review with a mentor.',
+    skills: ['Pragmatic Programmer', 'Code Review', 'Software Development', 'Time Management'],
   },
 ];
 
@@ -48,7 +45,7 @@ export function About({ theme, aboutRef }: { theme: Theme; aboutRef: MutableRefO
         or technologies while doing it.
       </p>
       <SubHeader title="EXPERIENCE" />
-      <div className="mb-24">
+      <div className="mb-24 flex flex-col gap-12">
         {experiences.map((experience) => (
           <ExperienceItem theme={theme} key={experience.time} experience={experience} />
         ))}
@@ -59,27 +56,34 @@ export function About({ theme, aboutRef }: { theme: Theme; aboutRef: MutableRefO
 
 function ExperienceItem({ theme, experience }: { theme: Theme; experience: Experience }): React.ReactNode {
   return (
-    <div className="flex flex-col sm:flex-row justify-between gap-4 py-2 sm:px-24">
-      <ul className="mt-8">
-        <li className="flex gap-4">
-          <div className="flex flex-col items-end flex-[2] text-right gap-1">
-            <span className={`${theme === 'light' ? 'text-black' : 'text-white'} text-xl sm:text-3xl font-medium`}>
-              {experience.company}
-            </span>
-            <span className="italic text-base sm:text-xl text-light-grey">{experience.time}</span>
-          </div>
-          <div className="flex flex-col items-center gap-2 pt-3">
-            <div className="w-4 h-4 rounded-full bg-accent" />
-            <div className="flex-grow w-1 rounded-full bg-accent" />
-          </div>
-          <div className="flex flex-col flex-[5] gap-1">
-            <span className={`${theme === 'light' ? 'text-black' : 'text-white'} text-xl sm:text-3xl font-medium`}>
-              {experience.position}
-            </span>
-            <p className="italic text-base sm:text-xl text-light-grey">{experience.description}</p>
-          </div>
-        </li>
-      </ul>
+    <div className="flex justify-between gap-4 py-2 sm:px-24">
+      <div className="flex flex-col items-end flex-[3] text-right gap-1">
+        <span className={`${theme === 'light' ? 'text-black' : 'text-white'} text-xl sm:text-3xl font-medium`}>
+          {experience.company}
+        </span>
+        <span className="italic text-base sm:text-xl text-light-grey">{experience.time}</span>
+      </div>
+      <div className="flex flex-col items-center gap-2 pt-3">
+        <div className="w-4 h-4 rounded-full bg-accent" />
+        <div className="flex-grow w-1 rounded-full bg-accent" />
+      </div>
+      <div className="flex flex-col flex-[5] gap-1">
+        <span className={`${theme === 'light' ? 'text-black' : 'text-white'} text-lg sm:text-3xl font-medium`}>
+          {experience.position}
+        </span>
+        <div className="flex flex-wrap gap-2 sm:gap-4 italic">
+          {experience.skills.map((skill) => (
+            <div
+              key={skill}
+              className={`${
+                theme === 'light' ? 'text-black' : 'text-white'
+              } border-2 border-accent py-1 px-4 rounded-full text-xs sm:text-base`}
+            >
+              {skill}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
