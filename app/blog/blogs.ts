752eb8803e9dea190df00d2fb955afb9e60f5602
Ryan Adi Putra <ryannadiputraa@gmail.com>
2024-05-06 16:28:15 +0800

    feat: blog list layout

---
+++ app/blog/blogs.ts
@@ -0,0 +1,6 @@
+export interface Blog {
+  title: string;
+  createdAt: Date;
+}
+
+export const blogs: Blog[] = [];
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,25 @@
+import Link from 'next/link';
+import { format } from 'date-fns';
+
+import { blogs } from './blogs';
+
 export default function Blog() {
-  return <p className="text-center">No post yet.</p>;
+  if (!blogs.length) {
+    return <div className="grid place-items-center">No post yet.</div>;
+  }
+  return (
+    <div className="flex flex-col gap-4">
+      {blogs.map((blog) => (
+        <div key={blog.title} className="flex items-center justify-between">
+          <div>
+            <span className="mr-4">{'>>'}</span>
+            <Link href={'/'} className="text-lg font-semibold border-b-2 border-b-transparent hover:border-b-accent">
+              {blog.title}
+            </Link>
+          </div>
+          <span className="text-sm">{format(blog.createdAt, 'MMM do, yyyy')}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
